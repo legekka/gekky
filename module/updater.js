763a471ae2d9c228f76663ce4df90b7ecd293290
legekka Yamori <legekka Yamori>
2017-03-14 01:33:47 +0100

    0.3.7-T4

--- module/updater.js
+++ module/updater.js
@@ -11,6 +11,29 @@ module.exports = () => {
             text = data.toString();
             if (text.indexOf('up-to-date') < 0) {
                 clearInterval(checker);
+                var need_full_reload = false;
+                var need_gekky_reload = false;
+                console.log('What Changed:');
+                whatchanged = exec('git log --name-status HEAD^..HEAD');
+                whatchanged.stdout.on('data', (data) => {
+                    var array = data.toString().split('\n');
+
+                    for (i in array) {
+                        if (array[i].indexOf('updater.js') >= 0 || array[i].indexOf('frame.js') >= 0) {
+                            need_full_reload = true;
+                        }
+                        if (array[i].indexOf('core.js') >= 0) {
+                            need_gekky_reload = true;
+                        }
+                    }
+                    if (!need_full_reload) {
+                        if (need_gekky_reload) {
+                            console.log('Gekky reload needed.');
+                        }
+                    } else {
+                        console.log('Full reload needed.');
+                    }
+                });
                 console.log('Updating...');
                 updateing = exec('git remote update');
                 updateing.stdout.on('data', (data) => {
@@ -19,7 +42,7 @@ module.exports = () => {
                 updateing.on('exit', (code) => {
                     console.log('exited: ' + code);
                 })
-            }   
+            }
         });
     }, 1000);
 }
