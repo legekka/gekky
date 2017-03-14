8122c2715c59d31181ff743dad30124b9e3fbe4a
legekka Yamori <legekka Yamori>
2017-03-14 13:38:35 +0100

    0.4.5.3 help.js: entrylist fix

--- module/help.js
+++ module/help.js
@@ -7,17 +7,14 @@ var path = './module/command.js';
 module.exports = {
     list: (callback) => {
         var entrylist = [];
-        var entry = {
-            'cmd': '',
-            'desc': ''
-        }
         text = fs.readFileSync(path).toString().split('\n');
         for (i in text) {
             text[i] = text[i].trim();
             if (text[i].startsWith('// !')) {
-                entry.cmd = text[i].split('|')[0].substr(3);
-                entry.desc = text[i].split('|')[1];
-                entrylist.push(entry);
+                entrylist.push({
+                    'cmd': text[i].split('|')[0].substr(3),
+                    'desc': text[i].split('|')[1]
+                });
             }
         }
         return callback(entrylist);
