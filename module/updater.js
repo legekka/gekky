cafa9cdb1db3ce1a031742dbd6b862eb773753be
legekka Yamori <legekka Yamori>
2017-03-14 02:40:52 +0100

    0.3.7-T24

--- module/updater.js
+++ module/updater.js
@@ -2,28 +2,26 @@
 // checking and updating from github if needed
 
 var exec = require('child_process').exec;
-var need_update = false;
-var need_full_reload = false;
-var need_core_reload = false;
+var resp = {
+    'update': false,
+    'full': false,
+    'core': false
+}
 
 module.exports = (callback) => {
     statuscheck = exec('git pull origin master');
     statuscheck.stdout.on('data', (data) => {
         text = data.toString();
         if (text.indexOf('up-to-date') < 0) {
-            need_update = true;
+            resp.update = true;
             if (data.toString().indexOf('frame.js') >= 0) {
-                need_full_reload = true;
+                resp.full = true;
             }
             if (data.toString().indexOf('core.js') >= 0 ||
                 data.toString().indexOf('console.js') >= 0) {
-                need_core_reload = true;
+                resp.core = true;
             }
         }
-        return callback({
-            'update': need_update,
-            'full': need_full_reload,
-            'core': need_core_reload
-        })
+        return callback(resp)
     });
 }
