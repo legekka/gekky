16eb15207475d4ce9f5d3546cf2c352ad03da9e9
legekka Yamori <legekka Yamori>
2017-03-14 01:11:02 +0100

    0.3.7-T2 Nem fogom végig kommentezni csak ha lényeges

--- module/updater.js
+++ module/updater.js
@@ -6,7 +6,14 @@ var exec = require('child_process').exec;
 module.exports = () => {
     setInterval(() => {
         console.log('checking git status.');
-        statuscheck = exec('git status -s');
-        statuscheck.stdout.on('data', (data) => {console.log(data)});
+        statuscheck = exec('git status');
+        statuscheck.stdout.on('data', (data) => {
+            text = data.toString();
+            if (text.indexOf('up-to-date') >= 0) {
+                console.log('up-to-date');
+            } else {
+                console.log('not up-to-date');
+            }
+        });
     }, 1000);
 }
