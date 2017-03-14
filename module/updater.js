48e3170c83fe4c544502208cfa00c027f9c94ec6
legekka Yamori <legekka Yamori>
2017-03-14 01:53:28 +0100

    0.3.7-T9

--- module/updater.js
+++ module/updater.js
@@ -5,8 +5,9 @@ var exec = require('child_process').exec;
 
 module.exports = () => {
     var checker = setInterval(() => {
-        statuscheck = exec('git checkout master');
+        statuscheck = exec('git pull origin master');
         statuscheck.stdout.on('data', (data) => {
+            console.log('data: ' + data);
             text = data.toString();
             if (text.indexOf('up-to-date') < 0) {
                 clearInterval(checker);
