4809574f8b290c8c4482edcb4874c0804c90afe4
legekka <legekka@legekka@gmail.com>
2017-04-16 23:54:11 +0200

    1.5.3 updater.js: core újraindítást igénylő modulok listájának bővítése

--- module/updater.js
+++ module/updater.js
@@ -67,7 +67,10 @@ module.exports = {
                 resp.full = true;
             }
             if (output.indexOf('core.js') >= 0 ||
-                output.indexOf('console.js') >= 0) {
+                output.indexOf('console.js') >= 0 ||
+                output.indexOf('cachemanager.js') >= 0 || 
+                output.indexOf('yrexia.js') >= 0 || 
+                output.indexOf('osutrack.js') >= 0) {
                 resp.core = true;
             }
             if (output.indexOf('osuirc.js') >= 0) {
