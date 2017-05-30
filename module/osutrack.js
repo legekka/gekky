82174f0ffaf2b48de9f4aa89edd88914c4488c65
legekka <legekka@gmail.com>
2017-05-30 17:22:09 +0200

    1.8.1.4 debug

--- module/osutrack.js
+++ module/osutrack.js
@@ -52,21 +52,20 @@ function checkForNewScores(core) {
                             if (output[i].rank != 'F') {
                                 var filePath = '../cache/' + fnamefix(output[i].user_id + '_' + output[i].date) + '.png';
                                 createPlayCard(output[i], playcard => {
-                                    if (ppvalue > 300) {
-                                        reqreload('./playcard.js')(playcard, filePath).then(() => {
-                                            reqreload('./webpconvert.js').file(filePath, (filep) => {
-                                                console.log(playcard.play.pp);
-                                                var ppvalue = parseFloat(playcard.play.pp.substr(0, playcard.play.pp.length - 2));
-                                                console.log(ppvalue);
-                                                if (ppvalue > 300) {
-                                                    core.bot.channels.get(core.ch.hun_scores).sendFile(filep);
-                                                } else {
-                                                    core.bot.channels.get(core.ch.hun_scorespam).sendFile(filep);
-                                                }
-                                                console.log(c.green('[OT] ') + reqreload('./getTime.js')() + ' | New score by ' + playcard.player.username);
-                                            });
+                                    reqreload('./playcard.js')(playcard, filePath).then(() => {
+                                        reqreload('./webpconvert.js').file(filePath, (filep) => {
+                                            console.log(playcard.play.pp);
+                                            var ppvalue = parseFloat(playcard.play.pp.substr(0, playcard.play.pp.length - 2));
+                                            console.log(ppvalue);
+                                            if (ppvalue > 300) {
+                                                core.bot.channels.get(core.ch.hun_scores).sendFile(filep);
+                                            } else {
+                                                core.bot.channels.get(core.ch.hun_scorespam).sendFile(filep);
+                                            }
+                                            console.log(c.green('[OT] ') + reqreload('./getTime.js')() + ' | New score by ' + playcard.player.username);
                                         });
-                                    }
+                                    });
+
                                 });
                             }
                             addAsOld(output[i]);
