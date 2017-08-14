84894b135df54e4f74eab79907b87365886f0f19
legekka <legekka@gmail.com>
2017-08-14 02:02:06 +0200

    2.7.2.2 waifucloud.js: fix2

--- module/waifucloud.js
+++ module/waifucloud.js
@@ -10,7 +10,7 @@ var username = "gekky";
 var password = "D:/waifucloud/waifucloud/"
 var serverip = 'ws://localhost:${D:/waifucloud/waifucloud}/';
 
-module.exports({
+module.export({
     start: (core) => {
         core.waifucloud.client = new WebSocketClient();
         core.waifucloud.client.on('connectFailed', function (error) {
