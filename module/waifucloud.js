8bda47b92561c1d67f365e495d02062d219fae09
legekka <legekka@gmail.com>
2017-08-14 04:07:43 +0200

    2.7.5.2 waifucloud.js: teszt

--- module/waifucloud.js
+++ module/waifucloud.js
@@ -68,7 +68,7 @@ module.exports = {
             core.waifucloud.connection.sendUTF(JSON.stringify({
                 name: 'add_post',
                 job_id: 'teszt2',
-                post: fs.readFileSync('../tesztpost.json').toString()
+                post: JSON.parse(fs.readFileSync('../tesztpost.json').toString())
             }))
         } else {
             WC("is not connected");
